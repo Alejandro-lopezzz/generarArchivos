c2e601bf6325d77e7d2bf8a3a3898849ab921d4a
Alejandro-lopezzz <xarteagax123@gmail.com>
2025-05-20 08:45:32 -0500

    generador de archivos

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const fs = require('fs/promises');
+const path = require('path');
+
+async function generarArchivo(i) {
+    const contenido = `Este es el archivo número ${i}`;
+    const nombre = path.join(__dirname, 'archivos', `archivo_${i}.txt`);
+    await fs.writeFile(nombre, contenido);
+}
+
+async function generarMuchosArchivos(cantidad) {
+    const carpeta = path.join(__dirname, 'archivos');
+
+   
+    await fs.mkdir(carpeta, { recursive: false });
+
+    const promesas = [];
+    for (let i = 1; i <= cantidad; i++) {
+        promesas.push(generarArchivo(i));
+    }
+
+
+    await Promise.all(promesas);
+    console.log(`✅ Se generaron ${cantidad} archivos.`);
+}
+
+generarMuchosArchivos(1000); 
